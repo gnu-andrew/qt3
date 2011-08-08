fa200add60ceba28bb21d680e902884da268152a
Andrew John Hughes <gnu_andrew@member.fsf.org>
2011-08-08 23:43:00 +0100

    Fix uic includehint errors

--- tools/designer/uic/form.cpp
+++ tools/designer/uic/form.cpp
@@ -738,6 +738,13 @@ void Uic::createFormImpl( const QDomElement &e )
             while ( !n2.isNull() ) {
                 if ( n2.tagName() == "includehint" ) {
                     QString file = n2.firstChild().toText().data();
+                    int colons = file.find("::");
+
+                    if (colons != -1)
+                    {
+                        file = file.right(file.length() - colons - 2);
+                    }
+
                     localIncludes += file;
                 }
                 n2 = n2.nextSibling().toElement();
